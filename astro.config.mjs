a2b892663a979e064abbe245dc36d82e92e6cca5
Shaif Arfan <shaifarfan08@gmail.com>
2024-02-19 22:41:53 +0600

    update favicons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
-
+import sitemap from '@astrojs/sitemap';
 const siteURL =
   import.meta.env.CONTEXT !== 'production' && process.env.DEPLOY_PRIME_URL;
 
 // https://astro.build/config
 export default defineConfig({
   site: siteURL || 'https://shaifarfan.com',
-  integrations: [react(), tailwind()],
+  integrations: [react(), tailwind(), sitemap()],
 });
