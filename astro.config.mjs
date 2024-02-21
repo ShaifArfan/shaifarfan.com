4af02f2e4a4c6b5bb0a5d8e9f6a8faf89b2907e4
Shaif Arfan <shaifarfan08@gmail.com>
2024-02-21 14:33:40 +0600

    update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,9 @@ import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 const siteURL =
-  import.meta.env.CONTEXT !== 'production' && process.env.DEPLOY_PRIME_URL;
+  process.env.CONTEXT === 'production'
+    ? process.env.URL
+    : process.env.DEPLOY_PRIME_URL;
 
 // https://astro.build/config
 export default defineConfig({
