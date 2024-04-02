62575ad2bec9d897bd73afdfd84887f3c45d8d6c
Shaif Arfan <shaifarfan08@gmail.com>
2024-04-02 22:29:37 +0600

    initial finish eslint blog

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,5 +1,5 @@
 import dayjs from 'dayjs';
 
 export const formattedPublishDate = (date: string | Date) => {
-  return dayjs(date).format('MMM D, YYYY');
+  return dayjs(date).format('MMM DD, YYYY');
 };
