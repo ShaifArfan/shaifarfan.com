fc734c0d3206e067b6c24fb555c4db1673e587aa
Shaif Arfan <shaifarfan08@gmail.com>
2024-12-16 01:28:12 +0600

    added tags & tag filter on blog

--- src/utils/getBlog.ts
+++ src/utils/getBlog.ts
@@ -1,11 +1,47 @@
 import type { CollectionEntry } from 'astro:content';
 import { isNetlifyContextProd } from './isNetlifyContextProd';
 
-export const getBlogPosts = (posts: CollectionEntry<'blog'>[]) => {
-  return posts.filter((post) => {
+export const getBlogPosts = (
+  posts: CollectionEntry<'blog'>[],
+  { tagFilter }: { tagFilter?: string[] }
+) => {
+  console.log(tagFilter);
+  const validPost = posts.filter((post) => {
     if (isNetlifyContextProd && post.slug.startsWith('draft-')) {
       return false;
     }
     return true;
   });
+  const filteredPosts = validPost
+    .filter((post) => {
+      if (
+        tagFilter &&
+        tagFilter.length > 0 &&
+        !post.data.tags.some((tag) =>
+          tagFilter.includes(tag.toLocaleLowerCase())
+        )
+      ) {
+        return false;
+      }
+      return true;
+    })
+    .sort(
+      (a, b) =>
+        new Date(b.data.pubDate).getTime() - new Date(a.data.pubDate).getTime()
+    );
+
+  const tags = validPost
+    .reduce((acc, post) => {
+      post.data.tags.forEach((tag) => {
+        if (acc.includes(tag.toLowerCase())) return;
+        acc.push(tag.toLowerCase());
+      });
+      return acc;
+    }, [] as string[])
+    .sort();
+
+  return {
+    posts: filteredPosts,
+    tags,
+  };
 };
